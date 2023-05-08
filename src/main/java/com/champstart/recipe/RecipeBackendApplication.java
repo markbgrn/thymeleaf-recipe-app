9f0de6a2e74d1a32c8e3a545313eadc5cd370c06
markbugarin <markbgrn@gmail.com>
2023-05-08 15:46:10 +0800

    Initial Commit

---
+++ src/main/java/com/champstart/recipe/RecipeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.champstart.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/champstart/recipe/RecipeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.champstart.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
