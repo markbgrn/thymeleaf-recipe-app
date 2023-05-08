f42b5a86e7499fff5a4f74c5c91622d28fb9c2e7
markbugarin <markbgrn@gmail.com>
2023-05-08 18:04:09 +0800

    Revert "add model of recipe,category,procedure,ingredient & add repository of recipe,category,procedure,ingredient"
    This reverts commit bbc2d7a62fc8fa796d04855b1e42b9e7733161f3.

--- src/main/java/com/champstart/recipeapp/category/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.champstart.recipeapp.category.repository;
-
-import com.champstart.recipeapp.category.model.CategoryModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<CategoryModel, Long> {
-}
