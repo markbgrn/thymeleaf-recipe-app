207c8647f4b584c0f12adcc5831899d1c513ca41
leonardnarvaez <79395144+leonardnarvaez@users.noreply.github.com>
2023-05-08 16:31:28 +0800

    Added User and Role repository files. Updated UserModel

---
+++ src/main/java/com/champstart/recipeapp/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.champstart.recipeapp.user.repository;
+
+import com.champstart.recipeapp.user.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    UserModel findByVerificationId(String verificationId);
+
+    UserModel findByEmail(String email);
+
+    UserModel findFirstByEmail(String email);
+}
