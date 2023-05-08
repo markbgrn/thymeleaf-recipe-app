2c22ece88d5de8f3df84e5eb2cccb83b5083a424
Remer <remer.irineo@gmail.com>
2023-05-08 16:17:19 +0800

    Added Model for User

---
+++ src/main/java/com/champstart/recipeapp/user/model/UserModel.java
@@ -0,0 +1,33 @@
+package com.champstart.recipeapp.user.model;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "users")
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String firstname;
+    private String lastname;
+    @Column(name = "verification_id")
+    private String verificationId;
+    @Column(name = "is_verified")
+    private Boolean isVerified;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+}
