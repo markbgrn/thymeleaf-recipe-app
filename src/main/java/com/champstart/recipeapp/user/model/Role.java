08ae71fa7301153eb6eb21fc521b105c7f0785a8
leonardnarvaez <79395144+leonardnarvaez@users.noreply.github.com>
2023-05-08 16:42:35 +0800

    Modified ManyToMany annotation into OneToOne

--- src/main/java/com/champstart/recipeapp/user/model/Role.java
+++ src/main/java/com/champstart/recipeapp/user/model/Role.java
@@ -18,6 +18,6 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @ManyToMany(mappedBy = "roles")
+    @OneToOne(mappedBy = "roles")
     private List<UserModel> users = new ArrayList<>();
 }
