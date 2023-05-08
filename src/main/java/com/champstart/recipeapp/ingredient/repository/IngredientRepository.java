3381f309cae8e83bf260162ae9b46bff48adfd23
markbugarin <markbgrn@gmail.com>
2023-05-08 21:28:21 +0800

    remove model and repository of recipe.

--- src/main/java/com/champstart/recipeapp/ingredient/repository/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.champstart.recipeapp.ingredient.repository;
-
-import com.champstart.recipeapp.ingredient.model.IngredientModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientRepository extends JpaRepository<IngredientModel, Long> {
-}
--- src/main/java/com/champstart/recipeapp/procedure/repository/ProcedureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.champstart.recipeapp.procedure.repository;
-
-import com.champstart.recipeapp.procedure.model.ProcedureModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProcedureRepository extends JpaRepository<ProcedureModel, Long> {
-}
