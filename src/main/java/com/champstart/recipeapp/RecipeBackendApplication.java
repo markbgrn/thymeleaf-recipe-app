61db856a6466323bd312fae3c2b474da6a74a9f4
markbugarin <markbgrn@gmail.com>
2023-05-08 16:06:06 +0800

    Refactor package name

--- src/main/java/com/champstart/recipeapp/RecipeBackendApplication.java
+++ src/main/java/com/champstart/recipeapp/RecipeBackendApplication.java
@@ -1,4 +1,4 @@
-package com.champstart.recipe;
+package com.champstart.recipeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/champstart/recipeapp/RecipeBackendApplicationTests.java
+++ src/test/java/com/champstart/recipeapp/RecipeBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.champstart.recipe;
+package com.champstart.recipeapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
