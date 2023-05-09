8bb4731370a7614fa6db6efe0252e72b1ec690b9
leonardnarvaez <79395144+leonardnarvaez@users.noreply.github.com>
2023-05-09 15:07:01 +0800

    Added User to Role relationship. Added User to Comment relationship.

--- src/main/java/com/champstart/recipeapp/user/model/UserModel.java
+++ src/main/java/com/champstart/recipeapp/user/model/UserModel.java
@@ -1,11 +1,14 @@
 package com.champstart.recipeapp.user.model;
 
+import com.champstart.recipeapp.comment.model.CommentModel;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Setter
 @Getter
@@ -28,4 +31,17 @@ public class UserModel {
     private LocalDateTime createdOn;
     @UpdateTimestamp
     private LocalDateTime updatedOn;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")}
+    )
+    private List<Role> roles = new ArrayList<>();
+
+    @OneToMany(mappedBy = "users")
+    private List<CommentModel> comments = new ArrayList<>();
+
+
 }
