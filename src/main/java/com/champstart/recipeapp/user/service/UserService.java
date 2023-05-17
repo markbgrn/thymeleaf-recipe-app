3159dae36d7559c79c2195055b5eb602ee665d66
Remer <remer.irineo@gmail.com>
2023-05-17 17:00:40 +0800

    Tests for User Profile and Reset Password. Removed unused methods.

--- src/main/java/com/champstart/recipeapp/user/service/UserService.java
+++ src/main/java/com/champstart/recipeapp/user/service/UserService.java
@@ -10,10 +10,7 @@ public interface UserService {
     UserModel saveUser(UserDto userDto);
     UserModel setUserVerified(String verificationId);
     UserModel findByEmail(String email);
-    UserModel mapToUser(UserDto userDto);
     UserModel findByVerificationId(String verificationId);
-    void updatePassword(UserDto userDto, String newPassword);
-
-    public UserModel updateProfile(UserModel userModel, String firstName, String lastName, String photoPath);
+    UserModel updatePassword(UserDto userDto, String newPassword);
+    UserModel updateProfile(UserModel userModel, String firstName, String lastName, String photoPath);
 }
-
