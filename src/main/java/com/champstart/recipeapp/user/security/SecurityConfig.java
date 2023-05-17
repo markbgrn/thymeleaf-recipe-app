60673191c5eabf0ccb27153d75c6cb264d9503e3
leonardnarvaez <79395144+leonardnarvaez@users.noreply.github.com>
2023-05-17 10:24:28 +0800

    Modified security config to prevent authorized users from accessing the login page. Updated navbar layouts for various HTML templates

--- src/main/java/com/champstart/recipeapp/user/security/SecurityConfig.java
+++ src/main/java/com/champstart/recipeapp/user/security/SecurityConfig.java
@@ -4,16 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.ui.DefaultLoginPageGeneratingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig{
     private CustomUsersDetailsService userDetailsService;
 
     @Autowired
@@ -21,17 +34,35 @@ public class SecurityConfig {
         this.userDetailsService = userDetailsService;
     }
 
+    class LoginPageFilter extends GenericFilterBean {
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+            if (SecurityContextHolder.getContext().getAuthentication() != null && SecurityContextHolder.getContext().getAuthentication().isAuthenticated() && ((HttpServletRequest)request).getRequestURI().equals("/login")) {
+                ((HttpServletResponse)response).sendRedirect("/home");
+            }
+            filterChain.doFilter(request, response);
+        }
+    }
+
+
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/login", "/register").anonymous()
+        http.addFilterBefore(new LoginPageFilter(), DefaultLoginPageGeneratingFilter.class);
+
+        http.httpBasic()
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().sameOrigin()
+                .and().formLogin().permitAll()
+                .and().authorizeRequests().antMatchers("/login").not().authenticated()
+                .antMatchers("/login", "/register", "/reset-password").anonymous()
                 .antMatchers("/css/**", "/js/**").permitAll()
                 .antMatchers("/home", "/recipes/**", "/comments/**", "/search/**", "/view-profile/**").authenticated()
+//                .and().authorizeRequests().anyRequest().authenticated()
                 .and()
                 .formLogin(form -> form
                         .loginPage("/login")
